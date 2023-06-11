334cdeec5cca5ebe33ca9a9bdfa143fb00718678
Nayem <nayemmh66@gmail.com>
2023-06-12 00:35:07 +0600

    Instructor verify middleware added and add class api created

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-require('dotenv').config();
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 3000;
-const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.n6ux36g.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://nayemmh66:uSVYH3knYa0ZFmHG@cluster0.n6ux36g.mongodb.net/?retryWrites=true&w=majority`;
 
 //middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+//verify jwt token
+const verifyJWT = (req,res,next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    res.status(404).send({error:true,"message":"Unauthorized Access"});
+  }
+  const token = authorization;
+  // console.log("Token received in jwt verify - ",token);
+  jwt.verify(token, process.env.ACCESS_TOKEN, (error,decoded)=>{
+    if(error){
+      res.status(403).send({error:true,"message":"Forbidden Access"});
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 app.get('/', (req, res) => {
     res.send("Summer Camp Is Running");
@@ -35,17 +53,44 @@ const client = new MongoClient(uri, {
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
       const userCollection = client.db("phero-assignment-12").collection("users");
+      const classCollection = client.db("phero-assignment-12").collection("classes");
 
 
-      app.post('/jwt',(req, res) => {
-        const data = req.body;
-
-        const token = jwt.sign(data,process.env.ACCESS_TOKEN);
-        console.log(data);
+      app.post('/jwt',  (req, res) => {
+        const user = req.body;
+        // console.log(user);
+        const token =  jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn : '1h'});
+        // console.log(token);
         res.send({token});
       })
 
-      //All user data
+      //verify the email is admin or not
+      const verifyAdmin = async (req, res, next) => {
+        const decodedEmail = req.decoded.email;
+        // console.log("email in admin verify" , email);
+        const query = {email:decodedEmail};
+        const user = await userCollection.findOne(query);
+        if(user.role != 0){
+          return res.status(403).send({ error: true, message: 'Forbidden Access' });
+        }
+        // console.log("Admin verified ",decodedEmail);
+        next();
+      }
+
+      //verify the email is instructor or not
+      const verifyInstructor = async (req, res, next) => {
+        const decodedEmail = req.decoded.email;
+        console.log("email in instructor verify" , decodedEmail);
+        const query = {email:decodedEmail};
+        const user = await userCollection.findOne(query);
+        if(user.role != 2){
+          return res.status(403).send({ error: true, message: 'Forbidden Access' });
+        }
+        console.log("Instructor verified ",decodedEmail);
+        next();
+      }
+
+      //Insert a new user if its not exist
       app.post('/user', async (req, res) => {
         const user = req.body;
         // console.log(user);
@@ -58,6 +103,11 @@ const client = new MongoClient(uri, {
         res.send(result);
       })
 
+      app.get('/user', verifyJWT, verifyAdmin, async (req, res) => {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      });
+
       //get a specefic user data
       app.get('/user/:email', async (req, res) => {
         const email = req.params.email;
@@ -65,6 +115,31 @@ const client = new MongoClient(uri, {
         const findUser = await userCollection.findOne(query);
         res.send(findUser);
       });
+
+      //update user role
+      app.put('/role/:email', verifyJWT, verifyAdmin, async (req, res) => {
+        const userEmail = req.params.email;
+        const role = req.body.role;
+        const filter = {email : userEmail};
+        const options = { upsert: false };
+        const update = { 
+          $set : { 
+            role : role, 
+          }
+        };
+        const result = await userCollection.updateOne(filter, update, options);
+
+      });
+
+
+      //Instructors Api
+      //Add a class
+      app.post('/class', verifyJWT, verifyInstructor, async (req, res) =>{
+        const classData = req.body;
+        const result = await classCollection.insertOne(classData);
+        res.send(result);
+      });
+
 
 
 
